7e2ff6a05d3d637451239cc148a59e61ba0cfb56
Wael Showair <show0017@algonquinlive.com>
2015-02-16 14:33:29 -0500

    reference animations using corresponding path id instead of n-th-child selector

--- js/main.js
+++ js/main.js
@@ -3,20 +3,20 @@ var hamburgerCross = {
 		url : 'svg/hamburger.svg',
 		animation : [
 			{
-				el : 'path:nth-child(1)',
+				el : '#upper-line',
 				animProperties : {
                     from : { val : '{"path" : "m 5.0916789,20.818994 53.8166421,0"}' },
 					to : { val : '{"path" : "M 12.972944,50.936147 51.027056,12.882035"}' }				}
 			},
 			{
-				el : 'path:nth-child(2)',
+				el : '#middle-line',
 				animProperties : {
 					from : { val : '{"transform" : "s1 1", "opacity" : 1}', before : '{"transform" : "s0 0"}' },
 					to : { val : '{"opacity" : 0}' }
 				}
 			},
 			{
-				el : 'path:nth-child(3)',
+				el : '#bottom-line',
 				animProperties : {
 					from : { val : '{"path" : "m 5.0916788,42.95698 53.8166422,0"}' },
 					to : { val : '{"path" : "M 12.972944,12.882035 51.027056,50.936147"}' }
